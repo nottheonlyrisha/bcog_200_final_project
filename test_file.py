29884b38a4c85a9b4c0cbe0163515724f761cb8a
nottheonlyrisha <rmahe@illinois.edu>
2025-03-27 10:05:43 -0500

    commented out outline

--- test_file.py
+++ test_file.py
@@ -1,23 +1,20 @@
-# test code will go here
-"""
-OUTLINE:
+# OUTLINE:
 
-main screen: tempo selection (type box), enter button, MAYBE select time signature and/or 4-8 counts to count in before tapping out rhythm
+# main screen: tempo selection (type box), enter button, MAYBE select time signature and/or 4-8 counts to count in before tapping out rhythm
 
-beat screen: selected count-in/4-beat count-in (either whole screen flashing or small icon - circle, square?)
-tapping screen: either screen or small icon flashing in and out whenever space is pressed, record rhythm (times between presses)
+# beat screen: selected count-in/4-beat count-in (either whole screen flashing or small icon - circle, square?)
+# tapping screen: either screen or small icon flashing in and out whenever space is pressed, record rhythm (times between presses)
 
-    MAYBE processing screen: "please wait", progress bar?
+#     MAYBE processing screen: "please wait", progress bar?
 
-final rhythm screen: show final notes based on tempo (and time signature?)
-    either in vector note form OR x form:
-      | | | | | |
-    -x-x-o-x-x-o
-    OR
-    --x--x--x----x--x--x----
-     |  |  |    |  |  |
+# final rhythm screen: show final notes based on tempo (and time signature?)
+#     either in vector note form OR x form:
+#       | | | | | |
+#     -x-x-o-x-x-o
+#     OR
+#     --x--x--x----x--x--x----
+#      |  |  |    |  |  |
 
-MAYBE note selection screen: bar highlighting each note, user types note name (maybe up/down to change notes, shift+up/down to change octaves?)
+# MAYBE note selection screen: bar highlighting each note, user types note name (maybe up/down to change notes, shift+up/down to change octaves?)
 
-MAYBE print/save screen: user can save final image (if not x form)
-"""
+# MAYBE print/save screen: user can save final image (if not x form)
