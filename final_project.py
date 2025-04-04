481afb3ddd4d9e9ff421e4a39072810235c73bed
nottheonlyrisha <rmahe@illinois.edu>
2025-04-04 12:13:24 -0500

    added error messages, changed tempo from buttons to two sliders

--- final_project.py
+++ final_project.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox #error messages!!
 
 def screen1(): #tempo screen
     main_window = tk.Tk()
@@ -9,8 +10,13 @@ def screen1(): #tempo screen
     
     def sumbit():
         tempo = tempo_entry.get()
-        main_window.destroy()
-        return tempo
+        if tempo == "":
+            messagebox.showinfo("error", "please enter a tempo")
+        elif int(tempo)/4 != int(tempo)//4:
+            messagebox.showinfo("error", "improper tempo: please enter a tempo divisible by 4")
+        else:
+            main_window.destroy()
+            return tempo
     
     submit_button = tk.Button(main_window, text="submit", command=sumbit)
     submit_button.grid(row=2)
@@ -21,28 +27,38 @@ def screen2(): #time signature screen
     main_window2 = tk.Tk()
 
     tk.Label(main_window2, text="time signature:").grid(row=0)
-    def time_signature_select(ts):
-        main_window2.destroy()
-        return ts
-    
-    time_signatures = ["2/4", "3/4", "4/4", "6/8"]
-    time_signature_buttons = []
-    for ts in time_signatures:
-        ts_button = tk.Button(main_window2, text=ts, command=lambda ts=ts: time_signature_select(ts))
-        time_signature_buttons.append(ts_button)
+    top_ts_slider = tk.Scale(main_window2, from_=2, to=8, orient=tk.HORIZONTAL, label="top:", activebackground="yellow")
+    bottom_ts_slider = tk.Scale(main_window2, from_=2, to=8, orient=tk.HORIZONTAL, label="bottom:", activebackground="yellow")
+    top_ts_slider.grid(row=1)
+    bottom_ts_slider.grid(row=2)
 
-    i = 0
-    for button in time_signature_buttons:
-        button.grid(row=1, column=i)
-        i += 1
+    def sumbit():
+        time_signature = f"{top_ts_slider.get()}/{bottom_ts_slider.get()}"
+        if time_signature not in ["2/4", "3/4", "4/4", "6/8"]:
+            messagebox.showinfo("error", "unsupported time signature")
+        else:
+            main_window2.destroy()
+
+
+    submit_button = tk.Button(main_window2, text="submit", command=sumbit)
+    submit_button.grid(row=3)
 
     main_window2.mainloop()
 
 def screen3(): #countdown select screen
     main_window3 = tk.Tk()
 
-    tk.Label(main_window3, text="countoff (beats):").grid(row=0)
-
+    countdown_slider = tk.Scale(main_window3, from_=2, to=8, orient=tk.HORIZONTAL, label="countoff (beats):", activebackground="yellow")
+    countdown_slider.grid(row=0)
+     
+    def sumbit():
+        countoff = countdown_slider.get()
+        main_window3.destroy()
+        print(countoff)
+    
+    submit_button = tk.Button(main_window3, text="submit", command=sumbit)
+    submit_button.grid(row=1)
+    
     main_window3.mainloop()
 
 def main():
