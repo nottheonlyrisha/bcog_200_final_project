c754db6a8aa1a8d9e509d8bfb7cf1b2cb2010f3a
nottheonlyrisha <rmahe@illinois.edu>
2025-04-04 11:19:56 -0500

    fixed error in screen2() where time signature selection screen wouldn't show up

--- final_project.py
+++ final_project.py
@@ -1,44 +1,54 @@
 import tkinter as tk
 
-def screen1():
+def screen1(): #tempo screen
     main_window = tk.Tk()
 
     tk.Label(main_window, text="tempo").grid(row=0)
     tempo_entry = tk.Entry(main_window, bg="white")
     tempo_entry.grid(row=1)
-
-    def tempo_select():
+    
+    def sumbit():
         tempo = tempo_entry.get()
         main_window.destroy()
         return tempo
     
-    submit_button = tk.Button(main_window, text="submit", command=tempo_select)
+    submit_button = tk.Button(main_window, text="submit", command=sumbit)
     submit_button.grid(row=2)
 
     main_window.mainloop()
 
-def screen2():
-    main_window = tk.Tk()
+def screen2(): #time signature screen
+    main_window2 = tk.Tk()
 
-    tk.Label(main_window, text="time signature:").grid(row=0)
-    def time_signature_select(i, j):
-        time_signature = f"{i}/{j}"
-        return time_signature
+    tk.Label(main_window2, text="time signature:").grid(row=0)
+    def time_signature_select(ts):
+        main_window2.destroy()
+        return ts
+    
     time_signatures = ["2/4", "3/4", "4/4", "6/8"]
     time_signature_buttons = []
-    for i in range(2,7):
-        ts_button = tk.Button(main_window, text=f"{i}/4", command=time_signature_select)
+    for ts in time_signatures:
+        ts_button = tk.Button(main_window2, text=ts, command=lambda ts=ts: time_signature_select(ts))
         time_signature_buttons.append(ts_button)
 
-    for i in range(len(time_signature_buttons)):
-        for button in time_signature_buttons:
-            button.grid(row=i)
+    i = 0
+    for button in time_signature_buttons:
+        button.grid(row=1, column=i)
+        i += 1
 
-    main_window.mainloop()
+    main_window2.mainloop()
+
+def screen3(): #countdown select screen
+    main_window3 = tk.Tk()
+
+    tk.Label(main_window3, text="countoff (beats):").grid(row=0)
+
+    main_window3.mainloop()
 
 def main():
     screen1()
     screen2()
+    screen3()
 
 if __name__ == "__main__":
     main()
